0335fe792c52a440f6ffdca004e777f4065bed0a
jeanninerodriguez <122111742+jeanninerodriguez@users.noreply.github.com>
2023-01-10 07:56:57 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+public class Main {
+ 
+  public static void main(String[] args) {
+    Persona persona = new Persona();
+    persona.setEdad(13);
+    persona.setNombre("Maria");
+    persona.setNumero(34567890);
+
+    System.out.println(persona.getEdad());
+    System.out.println(persona.getNombre());
+    System.out.println(persona.getNumero());
+  }
+}
+
+class Persona{
+  private int edad;
+  private String nombre;
+  private int numero;
+
+  public void setEdad(int edad){
+    this.edad = edad;
+  }
+
+  public int getEdad(){
+    return this.edad;
+  }
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+
+  public String getNombre(){
+    return this.nombre;
+  }
+  public void setNumero(int numero){
+    this.numero = numero;
+  }
+
+  public int getNumero(){
+    return this.numero;
+  }
+}
